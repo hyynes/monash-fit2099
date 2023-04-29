58568a3e0effb1056d12473922a7c811f3219911
kbay0009 <kbay0009@student.monash.edu>
2023-04-29 21:48:03 +1000

    Implemented the GustOfWind class successfully, added to map and groundFactory in application class.

--- src/game/environments/GustOfWind.java
+++ src/game/environments/GustOfWind.java
@@ -1,4 +1,32 @@
 package game.environments;
 
-public class GustOfWind {
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.RandomNumberGenerator;
+import game.enemies.LoneWolf;
+
+public class GustOfWind extends Ground {
+
+    private static Actor target;
+
+    public GustOfWind() {
+        super('&');
+    }
+
+    @Override
+    public void tick(Location location)
+    {
+        int randomNumber = RandomNumberGenerator.getRandomInt(100);
+        if (randomNumber < 33){
+            if (!location.containsAnActor()) {
+                location.addActor(new LoneWolf(target));
+            }
+        }
+    }
+
+    public static void setTarget(Actor actor) {
+        target = actor;
+    }
+
 }
