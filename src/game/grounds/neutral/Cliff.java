e1203fddca197b3faf0dc85514f2df7c5959fdbe
kbay0009 <kbay0009@student.monash.edu>
2023-05-15 16:44:57 +1000

    Added cliff and GoldenFogDoor grounds.

---
+++ src/game/grounds/neutral/Cliff.java
@@ -0,0 +1,15 @@
+package game.grounds.neutral;
+
+import edu.monash.fit2099.engine.positions.Ground;
+
+public class Cliff extends Ground {
+
+
+    /**
+     * Constructor.
+     *
+     */
+    public Cliff() {
+        super('+');
+    }
+}
---
+++ src/game/grounds/neutral/GoldenFogDoor.java
@@ -0,0 +1,14 @@
+package game.grounds.neutral;
+
+import edu.monash.fit2099.engine.positions.Ground;
+
+public class GoldenFogDoor extends Ground {
+
+    /**
+     * Constructor.
+     *
+     */
+    public GoldenFogDoor() {
+        super('D');
+    }
+}
